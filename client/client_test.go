5b93faeb4959559c0d081c37f0aa19ad504bb544
zhjsun <zhjsun@uuzu.com>
2022-03-03 21:25:02 +0800

    [feat] add quic stream

--- client/client_test.go
+++ client/client_test.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"context"
 	"crypto/tls"
 	"crypto/x509"
+	"encoding/binary"
 	"fmt"
+	"github.com/lucas-clemente/quic-go"
+	"io"
 	"net/http"
 	"os"
 	"testing"
@@ -12,11 +17,12 @@ import (
 )
 
 const (
-	URL = "https://127.0.0.1:8000/"
-	CA = "../cert/ca.crt"
+	URL  = "https://127.0.0.1:8000/"
+	ADDR = "127.0.0.1:8000"
+	CA   = "../cert/ca.crt"
 )
 
-func Test_QuicServer(t *testing.T) {
+func Test_httpServer(t *testing.T) {
 	rootCa, err := os.ReadFile(CA)
 	if err != nil {
 		panic("failed to read root certificate")
@@ -34,7 +40,7 @@ func Test_QuicServer(t *testing.T) {
 		},
 	}
 
-	client := &http.Client{Transport:tr}
+	client := &http.Client{Transport: tr}
 
 	_, err = client.Get(URL)
 	if err != nil {
@@ -43,7 +49,51 @@ func Test_QuicServer(t *testing.T) {
 	}
 }
 
-func Benchmark_QuicServer(b *testing.B) {
+func Test_echoServer(t *testing.T) {
+	rootCa, err := os.ReadFile(CA)
+	if err != nil {
+		panic("failed to read root certificate")
+	}
+
+	roots := x509.NewCertPool()
+	ok := roots.AppendCertsFromPEM(rootCa)
+	if !ok {
+		panic("failed to parse root certificate")
+	}
+
+	tlsConf := &tls.Config{
+		RootCAs:    roots,
+		NextProtos: []string{"quic-echo-example"},
+	}
+
+	session, err := quic.DialAddr(ADDR, tlsConf, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	stream, err := session.OpenStreamSync(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < 100; i++ {
+		msg := fmt.Sprintf("message%d", i)
+		buff := new(bytes.Buffer)
+		binary.Write(buff, binary.LittleEndian, int32(len(msg)))
+		buff.Write([]byte(msg))
+
+		_, err = stream.Write(buff.Bytes())
+		if err != nil {
+			panic(err)
+		}
+
+		pack := make([]byte, buff.Len() - 4)
+		io.ReadFull(stream, pack)
+		fmt.Println(string(pack))
+	}
+}
+
+func Benchmark_HttpServer(b *testing.B) {
 	rootCa, err := os.ReadFile(CA)
 	if err != nil {
 		panic("failed to read root certificate")
@@ -61,7 +111,7 @@ func Benchmark_QuicServer(b *testing.B) {
 		},
 	}
 
-	client := &http.Client{Transport:tr}
+	client := &http.Client{Transport: tr}
 
 	for i := 0; i < b.N; i++ {
 		_, err := client.Get(URL)
@@ -72,4 +122,45 @@ func Benchmark_QuicServer(b *testing.B) {
 	}
 }
 
+func Benchmark_StreamServer(b *testing.B) {
+	rootCa, err := os.ReadFile(CA)
+	if err != nil {
+		panic("failed to read root certificate")
+	}
 
+	roots := x509.NewCertPool()
+	ok := roots.AppendCertsFromPEM(rootCa)
+	if !ok {
+		panic("failed to parse root certificate")
+	}
+
+	tlsConf := &tls.Config{
+		RootCAs:    roots,
+		NextProtos: []string{"quic-echo-example"},
+	}
+
+	session, err := quic.DialAddr(ADDR, tlsConf, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	stream, err := session.OpenStreamSync(context.Background())
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < b.N; i++ {
+		msg := fmt.Sprintf("message%d", i)
+		buff := new(bytes.Buffer)
+		binary.Write(buff, binary.LittleEndian, int32(len(msg)))
+		buff.Write([]byte(msg))
+
+		_, err = stream.Write(buff.Bytes())
+		if err != nil {
+			panic(err)
+		}
+
+		pack := make([]byte, buff.Len() - 4)
+		io.ReadFull(stream, pack)
+	}
+}
--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
+	"crypto/tls"
+	"fmt"
+	"github.com/lucas-clemente/quic-go"
+	"github.com/lucas-clemente/quic-go/http3"
+	"io"
 	"log"
 	"net/http"
-
-	"github.com/lucas-clemente/quic-go/http3"
 )
 
 const (
@@ -14,12 +18,65 @@ const (
 	PRIV = "./cert/server.key"
 )
 
-func main() {
-	log.Println("start https server on", ADDR)
+type loggingWriter struct{ io.Writer }
 
+func main() {
+	log.Println("start quic server on", ADDR)
+
+	//httpServer()
+	echoServer()
+}
+
+func httpServer() {
 	http.Handle("/", http.FileServer(http.Dir(WEB_DIR)))
 	err := http3.ListenAndServeQUIC(ADDR, CERT, PRIV, nil)
 	if err != nil {
 		panic(err)
+	}
+}
+
+func echoServer() {
+	const pack_len = 4
+	var err error
+
+	certs := make([]tls.Certificate, 1)
+	certs[0], err = tls.LoadX509KeyPair(CERT, PRIV)
+	if err != nil {
+		panic(err)
+	}
+
+	config := &tls.Config{
+		Certificates: certs,
+		NextProtos:   []string{"quic-echo-example"},
+	}
+
+	listener, _ := quic.ListenAddr(ADDR, config, nil)
+	defer listener.Close()
+
+	for {
+		sess, _ := listener.Accept(context.Background())
+		stream, _ := sess.AcceptStream(context.Background())
+
+		go func(stream quic.Stream) {
+			defer stream.Close()
+			b := make([]byte, 32 * 1024)
+
+			for {
+				_, err := io.ReadAtLeast(stream, b, pack_len)
+				switch err {
+				case nil:
+				default:
+					fmt.Println(err)
+					return
+				}
+
+				l := uint32(b[0]) | uint32(b[1])<<8 | uint32(b[2])<<16 | uint32(b[3])<<24
+				pack := b[pack_len:pack_len+l]
+				fmt.Println(string(pack))
+
+				stream.Write(pack)
+			}
+
+		}(stream)
 	}
 }
